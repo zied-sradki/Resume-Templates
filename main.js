f258c45cef2878866dfd9538a4b2e31dd2cb5707
zied7 <zied.sradkii@gmail.com>
2020-10-19 18:26:23 +0100

    18h

--- main.js
+++ main.js
@@ -8,7 +8,23 @@ $("#changePicture").on("click", function (e) {
 });
 
 
-$("#addMyname").on("click", function (e) {
+$("#addSkill").on("click", function (e) {
+    e.preventDefault();
+    var skillValue = $("#skillValue").val();
+    var list = $("#skillList");
+    var item = $("<li class='skillItem' style='display:none'>" + skillValue + "</li>");
+    if (skillValue !== "" ) {
+      list.append(item);
+      $(".skillItem").fadeIn(1500);
+
+    } else {
+        alert("Please fill in all the inputs!");
+    }
+  });
+
+
+
+$("#addName").on("click", function (e) {
     var mynameVal = $("#myname").val();
     if (mynameVal !== "") {
         $("#name").text(mynameVal);
@@ -18,3 +34,20 @@ $("#addMyname").on("click", function (e) {
 });
 
 
+$("#addTitle").on("click", function (e) {
+    var mynameVal = $("#myTitle").val();
+    if (mynameVal !== "") {
+        $("#title").text(mynameVal);
+    } else {
+        alert("Please fill in all the inputs!");
+    }
+});
+
+$("#addDesc").on("click", function (e) {
+    var mynameVal = $("#myDesc").val();
+    if (mynameVal !== "") {
+        $("#desc").text(mynameVal);
+    } else {
+        alert("Please fill in all the inputs!");
+    }
+});
